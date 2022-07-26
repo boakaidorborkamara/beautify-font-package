a7513d9fad0332812e50684e8bbd8f5e0837bddc
Boakai <boakaidkamara@gmail.com>
2022-07-26 01:28:06 +0000

    setup module

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const font_beauty = (options) => {
 
+
+    {
+        color: "text-bloody"
+        decoration: "decorate-line-below"
+
+    }
+
+
+    //target all of the elements that have the class of the package
+    let targeted_items = document.querySelectorAll('.beautify-font');
+
+
+    //create style based on user selected values
+    if (options.color === "boody") { //colors
+        options.color = "red";
+    } else if (options.color === "sky") {
+        options.color = "blue";
+    }
+
+    if (options.decoration)
+
+
+        targeted_items.forEach((ele) => {
+
+        if (options.color) {
+            ele.style.color = options.color;
+        }
+
+    })
 }
